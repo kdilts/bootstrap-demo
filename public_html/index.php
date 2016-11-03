19d003e512e754edcf5825c83ba2202fa38027e2
Kevin Dilts <kdilts13@gmail.com>
2016-11-03 10:51:04 -0600

    added support for old ie browsers

--- public_html/index.php
+++ public_html/index.php
@@ -16,6 +16,15 @@
 
 		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+
+
 		<title>Bootstrap Demo</title>
 	</head>
 	<body>
