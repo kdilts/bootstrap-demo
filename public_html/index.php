420aee73f2ee213b4cdffffdfa92b97b9e1d7df2
Kevin Dilts <kdilts13@gmail.com>
2016-11-03 11:34:00 -0600

    added cat images created example layout using bootstrap grid

--- public_html/index.php
+++ public_html/index.php
@@ -29,6 +29,34 @@
 		<title>Bootstrap Demo</title>
 	</head>
 	<body>
-		<h1>Hello Bootstrap!</h1>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<h1>Welcome To My Bootstrap Demo</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-6">
+					<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum </p>
+				</div>
+				<div class="col-xs-6">
+					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet </p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-3">
+					<img src="images/cat1.jpg" alt="cat pic 1"/>
+				</div>
+				<div class="col-xs-3">
+					<img src="images/cat2.jpg" alt="cat pic 2"/>
+				</div>
+				<div class="col-xs-3">
+					<img src="images/cat3.jpg" alt="cat pic 3"/>
+				</div>
+				<div class="col-xs-3">
+					<img src="images/cat4.jpg" alt="cat pic 4"/>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
