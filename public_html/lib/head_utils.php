b4728beea07b035708cff4eebccafaa25dfc8cee
Kevin Dilts <kdilts13@gmail.com>
2016-11-03 13:06:27 -0600

    fixed warning in head_utils.php

--- public_html/lib/head_utils.php
+++ public_html/lib/head_utils.php
@@ -8,7 +8,7 @@
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<link rel="stylesheet" href="css/style.css" type="text/css"/>
+<link rel="stylesheet" href="../css/style.css" type="text/css"/>
 
 <!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
